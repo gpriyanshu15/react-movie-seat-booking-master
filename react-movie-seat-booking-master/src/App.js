0c4ba8fa9befd73da40d6d0670d7509ce5e8d727
Priyanshu Gupta <92108888+gpriyanshu15@users.noreply.github.com>
2023-05-05 15:56:08 +0530

    first commit

---
+++ react-movie-seat-booking-master/src/App.js
@@ -0,0 +1,38 @@
+import React, {useState} from "react"
+import "./App.css"
+
+import MovieSelector from "./components/MovieSelector"
+import SeatAvailability from "./components/SeatAvailability"
+import SeatMatrix from "./components/SeatMatrix"
+import PriceCalculator from "./components/PriceCalculator"
+
+import MovieContext from './contexts/MovieContext'
+
+const App = () => {
+
+	const [movies, EditMovies] = useState({
+		movieNames: {
+			"Bloodshot": 10,
+			"The girl on the Train": 8,
+			"The invisible Man": 11,
+			"Onward": 12,
+			"My Spy": 9
+		},
+		moviePrice: 10,
+		totalSeats: 0,
+		seatNumbers: []
+	})
+
+	return (
+		<div className="main container">
+			<MovieContext.Provider value={{ movies, changeState: EditMovies }}>
+				<MovieSelector />
+				<SeatMatrix />
+				<SeatAvailability />
+				<PriceCalculator />
+			</MovieContext.Provider>
+		</div>
+	)
+}
+
+export default App
